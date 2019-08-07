f6e3e6b0c9fb977f6e3a52078209ff159444f46a
axanovrl <axanov.rl@gmail.com>
2019-08-07 12:13:49 +0600

    commit_15

---
+++ src/pages/NavBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Navbar, Nav } from 'react-bootstrap';
+
+import DropdownNav from './DropdownNav';
+
+const NavBar = () =>{
+
+    return(
+        <Navbar bg="dark" expand="lg" variant ="dark">
+        <div className="container">
+      <Navbar.Brand href="/dashboard"><img src="/img/logo1.png" width="42" height="42"></img></Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+          <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+          <Nav.Link href="/contacts">Contacts</Nav.Link>
+          <Nav.Link href="/campaigns">Campaigns</Nav.Link>
+          </Nav>
+          <DropdownNav/>
+      </Navbar.Collapse>
+      </div>
+ 
+</Navbar>
+    )
+}
+
+export default NavBar;
