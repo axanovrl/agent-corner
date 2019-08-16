22363f6cf7171af49ae13a8d03a6104ba2c5a687
axanovrl <axanov.rl@gmail.com>
2019-08-16 15:07:55 +0600

    fix table

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
-import Contacts from './components/Contacts'
+import NavBar from '../../components/NavBar';
+import Table from './components/Table';
 
-const index = () =>{
-    return(
-        <Contacts/>
+const index = () => {
+    return (
+        <React.Fragment>
+            <NavBar />
+            <Container>
+                <div className="container">
+                    <div className="mb-2-hf d-flex justify-content-between flex-wrap">
+                        <h2>Contacts</h2>
+                    </div>
+                    <div className="mb-2-hf">
+                        <Table />
+                    </div>
+                </div>
+            </Container>
+        </React.Fragment>
+
+
     )
 }
 
