819516e1d42b683dda548c821073df6fe16202b9
axanovrl <axanov.rl@gmail.com>
2019-08-24 13:41:48 +0600

    fix table

--- src/pages/Contacts/components/const.js
+++ src/pages/Contacts/components/const.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 
-export const List1 = [
+export const LIST_CONTACTS = [
 	{
 		photo: "img/Photo.png",
 		name: "Jeffery Fox",
@@ -9,7 +9,47 @@ export const List1 = [
 		city: "San Francisco, CA",
 		price: "$450,000-$500,000",
 		date: "July 1, 2019"
-	}
+  },  
+  {
+    photo: "img/Photo.png",
+		name: "Jeffery Fox",
+		zipcode: "123456",
+		city: "San Francisco, CA",
+		price: "$450,000-$500,000",
+		date: "July 1, 2019"
+  },
+  {
+    photo: "img/Photo.png",
+		name: "Jeffery Fox",
+		zipcode: "123456",
+		city: "San Francisco, CA",
+		price: "$450,000-$500,000",
+		date: "July 1, 2019"
+  },
+  {
+    photo: "img/Photo.png",
+		name: "Jeffery Fox",
+		zipcode: "123456",
+		city: "San Francisco, CA",
+		price: "$450,000-$500,000",
+		date: "July 1, 2019"
+  },
+  {
+    photo: "img/Photo.png",
+		name: "Jeffery Fox",
+		zipcode: "123456",
+		city: "San Francisco, CA",
+		price: "$450,000-$500,000",
+		date: "July 1, 2019"
+  },
+  {
+    photo: "img/Photo.png",
+		name: "Jeffery Fox",
+		zipcode: "123456",
+		city: "San Francisco, CA",
+		price: "$450,000-$500,000",
+		date: "July 1, 2019"
+  }
 ]
 
 export const COLUMNS = [
