5e6b2d5d16ed560e238e2dd262b60bc5753e2ba0
axanovrl <axanov.rl@gmail.com>
2019-08-26 15:38:14 +0600

    fix

--- src/pages/SignUp/components/FieldSignUp.js
+++ src/pages/SignUp/components/FieldSignUp.js
@@ -1,41 +1,44 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { Container } from 'react-bootstrap';
 
 import FieldInput from '../../../components/FieldInput'
 
 const FieldSignUp = () => {
 
   return (
-    <form>
-      <div className="d-flex justify-content-center">
-        <div className="wizard-form">
-          <h2>Create your account</h2>
-          <div className="form-row">
-            <div className="form-group col">
-              <FieldInput label="First Name" className="form-label" placeholder="Sam" />
+    <Container>
+      <form>
+        <div className="d-flex justify-content-center">
+          <div className="wizard-form">
+            <h2>Create your account</h2>
+            <div className="form-row">
+              <div className="form-group col">
+                <FieldInput label="First Name" className="form-label" placeholder="Sam" />
+              </div>
+              <div className="form-group col">
+                <FieldInput label="Last Name" className="form-label" placeholder="Smith" />
+              </div>
             </div>
-            <div className="form-group col">
-              <FieldInput label="Last Name" className="form-label" placeholder="Smith" />
+            <div className="form-group">
+              <FieldInput label="Email" type="email" className="form-label" placeholder="sam_smith@gmail.com" />
             </div>
-          </div>
-          <div className="form-group">
-            <FieldInput label="Email" type="email" className="form-label" placeholder="sam_smith@gmail.com" />
-          </div>
-          <div className="form-group">
-            <FieldInput label="Password" type="password" className="form-label font-weigh-bold" />
-          </div>
-          <div className="form-group">
-            <FieldInput label="Confirm Password" type="password" className="form-label" />
-          </div>
-          <button type="submit">Sign Up</button>
-          <p>Already have an account? <Link to="/login">Sign in instead</Link></p>
-          <hr></hr>
-          <p>By continuing, you agree to our
+            <div className="form-group">
+              <FieldInput label="Password" type="password" className="form-label font-weigh-bold" />
+            </div>
+            <div className="form-group">
+              <FieldInput label="Confirm Password" type="password" className="form-label" />
+            </div>
+            <button type="submit">Sign Up</button>
+            <p>Already have an account? <Link to="/login">Sign in instead</Link></p>
+            <hr></hr>
+            <p>By continuing, you agree to our
             <a href="#">Terms of Use</a> and <a href="#">Privacy Policy</a>
-          </p>
+            </p>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </Container>
   )
 }
 
